a1a275d0ffb7ca1b0df76368074dc2be09ba99f9
Cedric <cedric.ringger@gmail.com>
2022-04-27 17:30:22 +0200

    Added functions to get some additional information used in the report Implemented parsing of required args

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
+import os
 import sys
+from fpdf import FPDF
 
-from src.alphaVantage import *
+from src.alphaVantage import get_stock_data, get_description
+from src.graphGenerator import create_graph
+from src.report import generate_new_page
 
 timePeriod = ""
 stockName = ""
@@ -13,10 +17,31 @@ ftpPassword = ""
 
 def check_args():
     if len(timePeriod) > 0 and len(stockName) > 0:
-        #print(getDescription(stockName))
-        (getStockData(stockName))
+        if timePeriod.lower().endswith("h") or timePeriod.lower().endswith("d"):
+            return True
+        else:
+            print("Invalid time please append H(hours) or D(days) to the end")
     else:
         print("Missing Argument -t or -a")
+    return False
+
+
+def rising(data):
+    start = data[list(data.keys())[-1]]["1. open"]
+    end = data[list(data.keys())[0]]["4. close"]
+    return end > start
+
+
+def get_diff(data):
+    start = data[list(data.keys())[-1]]["1. open"]
+    end = data[list(data.keys())[0]]["4. close"]
+    return round(float(end) - float(start), 2)
+
+
+def get_percentage(data):
+    diff = get_diff(data)
+    start = data[list(data.keys())[-1]]["1. open"]
+    return round(diff / float(start) * 100, 2)
 
 
 def main():
@@ -46,7 +71,18 @@ def main():
             if arg.startswith("-"):
                 print("Unknown Option " + arg)
                 exit(-1)
-    check_args()
+
+    if check_args():
+        pdf = FPDF()
+        companies = stockName.split(",")
+        for company in companies:
+            data = get_stock_data(company, timePeriod)
+            info = get_description(company)
+            if len(info) > 0 and len(data) > 0:
+                create_graph(data)
+                generate_new_page(pdf, info["Description"], company, info["Name"], rising(data), get_percentage(data), get_diff(data))
+        os.remove("stock.png")
+        pdf.output("report.pdf", "F")
 
 
 if __name__ == '__main__':
