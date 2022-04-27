14aa17a41ac43bea86b74b81953381538b761710
Cedric <cedric.ringger@gmail.com>
2022-04-27 17:28:55 +0200

    Removed unused code Changed name from candle to stock.png

--- src/graphGenerator.py
+++ src/graphGenerator.py
@@ -9,10 +9,6 @@ def print_bars(plt, index, close, high, low, open, stick1, stick2, body):
 
 
 def create_graph(data):
-    # data = get_stock_data("IBM")
-    # indexes = list(data)[:60]
-    # data = dict((k, data[k]) for k in indexes)
-
     prices = pd.DataFrame.from_dict(data, "index")
     prices["1. open"] = prices["1. open"].astype(float)
     prices["2. high"] = prices["2. high"].astype(float)
@@ -27,9 +23,9 @@ def create_graph(data):
     print_bars(plt, up.index, up["4. close"], up["2. high"], up["3. low"], up["1. open"], up["4. close"], up["1. open"], "green")
     print_bars(plt, down.index, down["4. close"], down["2. high"], down["3. low"], down["1. open"], down["1. open"], down["4. close"], "red")
 
-    plt.xticks(rotation=45, ha='right', fontsize=12)
-    plt.yticks(fontsize=12)
+    plt.xticks(rotation=45, ha='right', fontsize=14)
+    plt.yticks(fontsize=14)
     fig = matplotlib.pyplot.gcf()
-    fig.set_size_inches(18.5, 10.5)
+    fig.set_size_inches(17, 10)
 
-    plt.savefig("candle.png", bbox_inches="tight")
+    plt.savefig("stock.png", bbox_inches="tight")
