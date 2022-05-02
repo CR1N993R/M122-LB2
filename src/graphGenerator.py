b66a4dcc3e03b26ddf8df67a3f99bfcb199fe7cc
Cedric <cedric.ringger@gmail.com>
2022-05-02 18:59:26 +0200

    Fixed buggy graphs for hourly

--- src/graphGenerator.py
+++ src/graphGenerator.py
@@ -3,13 +3,20 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-def print_bars(plt, index, close, high, low, open, stick1, stick2, body):
-    plt.bar(index, close - open, .4, bottom=open, color=body)
-    plt.bar(index, high - stick1, .05, bottom=stick1, color="black")
-    plt.bar(index, low - stick2, .05, bottom=stick2, color="black")
+def print_bars(plt, index, close, high, low, open, stick1, stick2, body, size1, size2):
+    plt.bar(index, close - open, size1, bottom=open, color=body)  # .02 , .4
+    plt.bar(index, high - stick1, size2, bottom=stick1, color="black")  # .005, .05
+    plt.bar(index, low - stick2, size2, bottom=stick2, color="black")  # .005, .05
 
 
-def create_graph(data, company):
+def create_graph(data, company, days):
+    if days:
+        size1 = .4
+        size2 = .05
+    else:
+        size1 = .02
+        size2 = .005
+
     print("Generating graph for company: " + company + "...")
     prices = pd.DataFrame.from_dict(data, "index")
     prices["1. open"] = prices["1. open"].astype(float)
@@ -22,8 +29,8 @@ def create_graph(data, company):
     down = prices[prices["4. close"] < prices["1. open"]]
 
     plt.figure()
-    print_bars(plt, up.index, up["4. close"], up["2. high"], up["3. low"], up["1. open"], up["4. close"], up["1. open"], "green")
-    print_bars(plt, down.index, down["4. close"], down["2. high"], down["3. low"], down["1. open"], down["1. open"], down["4. close"], "red")
+    print_bars(plt, up.index, up["4. close"], up["2. high"], up["3. low"], up["1. open"], up["4. close"], up["1. open"], "green", size1, size2)
+    print_bars(plt, down.index, down["4. close"], down["2. high"], down["3. low"], down["1. open"], down["1. open"], down["4. close"], "red", size1, size2)
 
     plt.xticks(rotation=45, ha='right', fontsize=14)
     plt.yticks(fontsize=14)
